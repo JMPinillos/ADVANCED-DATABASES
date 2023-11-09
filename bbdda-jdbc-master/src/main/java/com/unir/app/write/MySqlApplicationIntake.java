0942acded78e9de60a19a148bd44a5012305d7a3
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2023-11-09 14:47:19 +0100

    Update MySqlApplicationIntake.java

--- bbdda-jdbc-master/src/main/java/com/unir/app/write/MySqlApplicationIntake.java
+++ bbdda-jdbc-master/src/main/java/com/unir/app/write/MySqlApplicationIntake.java
@@ -89,8 +89,8 @@ public class MySqlApplicationIntake {
 
                 if (province == null) {
                     province = new MySqlProvinces (
-                            (provinces.size()+1),
-                            nextLine[0]
+                            (provinces.size()+1),   // ID segun el contenido de la tabla.
+                            nextLine[0]             // Cogemos el dato de la columna provincia. 
                     );
                     provinces.add(province);
                 }
@@ -106,9 +106,8 @@ public class MySqlApplicationIntake {
 
                 if (province == null) {
                     MySqlMunicipalities municipalitie = new MySqlMunicipalities(
-                            (municipalities.size()+1),
-                            (provinces.size()+1),
-                            nextLine[1]
+                            (municipalities.size()+1),  // ID segun el contenido de la tabla. 
+                            nextLine[1]                 // Cogemos el dato de la columna municipio. 
                     );
                     municipalities.add(municipalitie);
                 }
@@ -172,8 +171,10 @@ public class MySqlApplicationIntake {
 
     private static void intakeMunicipalities(Connection connection, List<MySqlMunicipalities> municipalities) throws SQLException {
 
+        // Consultas de la tabla provincias para coger el id
+        String selectSqlProvinces = "SELECT id_pro FROM provinces WHERE name = ?";
+
         // Consultas de la tabla Municipios
-        String selectSqlMunicipalities = "SELECT COUNT(*) FROM muni WHERE name = ?";
         String insertSqlMunicipalities = "INSERT INTO municipalities (mun_id, pro_id, name)"
                 + "VALUES (?, ?, ?)";
 
@@ -181,6 +182,7 @@ public class MySqlApplicationIntake {
         int contador = 0;
 
         // Preparamos las consultas, una unica vez para poder reutilizarlas en el batch
+        PreparedStatement selectStatementIdProvincesMun = connection.prepareStatement(selectSqlProvinces);
         PreparedStatement insertStatementMunicipalities = connection.prepareStatement(insertSqlMunicipalities);
 
         // Desactivamos el autocommit para poder ejecutar el batch y hacer commit al final
@@ -194,7 +196,7 @@ public class MySqlApplicationIntake {
 
             ResultSet resultSet = selectStatementMunicipalities.executeQuery();
             resultSet.next(); // Nos movemos a la primera fila
-            int rowCount = resultSet.getInt(1);
+            int rowCount = resultSet.getPro_id;
 
             // Si no existe, insertamos. Si existe, no hacemos nada.
             if(rowCount == 0) {
@@ -229,206 +231,15 @@ public class MySqlApplicationIntake {
         statement.setString(2, provinces.getName());
     }
 
+    private static void fillSelectStatementIdProvToIdMun(PreparedStatement statement, MySqlProvinces provinces) throws SQLException {
+        statement.setString(2, provinces.getName());
+    }
+
     private static void fillInsertStatementMunicipalities(PreparedStatement statement, MySqlMunicipalities municipalities) throws SQLException {
         statement.setInt(1, municipalities.getMun_id());
-        statement.setInt(2, municipalities.getPro_id());
-        statement.setString(3, municipalities.getPro_id());
-    }
-/*
-    private static List<MySqlFuel> readData() {
-
-        // Try-with-resources. Se cierra el reader automáticamente al salir del bloque try
-        // CSVReader nos permite leer el fichero CSV linea a linea
-        try (CSVReader reader = new CSVReaderBuilder(
-                new FileReader("Empleados.csv"))
-                .withCSVParser(new CSVParserBuilder()
-                        .withSeparator(',').build()).build()) {
-
-            // Creamos la lista de empleados y el formato de fecha
-            List<MySqlFuel> employees = new LinkedList<>();
-            SimpleDateFormat format = new SimpleDateFormat("yyy-MM-dd");
-
-            // Saltamos la primera linea, que contiene los nombres de las columnas del CSV
-            reader.skip(1);
-            String[] nextLine;
-
-            // Leemos el fichero linea a linea
-            while((nextLine = reader.readNext()) != null) {
-
-                // Creamos el empleado y lo añadimos a la lista
-                MySqlFuel employee = new MySqlFuel(
-                        Integer.parseInt(nextLine[0]),
-                        nextLine[1],
-                        nextLine[2],
-                        nextLine[3],
-                        new Date(format.parse(nextLine[4]).getTime()),
-                        new Date(format.parse(nextLine[5]).getTime()),
-                        nextLine[6],
-                        new Date(format.parse(nextLine[7]).getTime()),
-                        new Date(format.parse(nextLine[8]).getTime())
-                );
-                employees.add(employee);
-            }
-            return employees;
-        } catch (IOException e) {
-            log.error("Error al leer el fichero CSV", e);
-            throw new RuntimeException(e);
-        } catch (CsvValidationException | ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Introduce los datos en la base de datos.
-     * Si el empleado ya existe, se actualiza.
-     * Si no existe, se inserta.
-     *
-     * Toma como referencia el campo emp_no para determinar si el empleado existe o no.
-     * @param connection - Conexión a la base de datos
-     * @param employees - Lista de empleados
-     * @throws SQLException - Error al ejecutar la consulta
-     */
-/*    private static void intake(Connection connection, List<MySqlFuel> employees) throws SQLException {
-
-        // Consultas para los empleados
-        String selectSql = "SELECT COUNT(*) FROM employees WHERE emp_no = ?";
-        String insertSql = "INSERT INTO employees (emp_no, first_name, last_name, gender, hire_date, birth_date) "
-                + "VALUES (?, ?, ?, ?, ?, ?)";
-        String updateSql = "UPDATE employees SET first_name = ?, last_name = ?, gender = ?, hire_date = ?, birth_date = ? WHERE emp_no = ?";
-
-        // Consultas para la tabla "dept_emp", que relaciona a los empleados con los departamentos.
-        String selectSqlDeptEmp = "SELECT COUNT(*) FROM dept_emp WHERE emp_no = ? and dept_no = ?";
-        String insertSqlDeptEmp = "INSERT INTO dept_emp (emp_no, dept_no, from_date, to_date) "
-                + "VALUES (?, ?, ?, ?)";
-        String updateSqlDeptEmp = "UPDATE dept_emp SET from_date = ?, to_date = ? WHERE emp_no = ? and dept_no = ?";
-
-        int lote = 5;
-        int contador = 0;
-
-        // Preparamos las consultas, una unica vez para poder reutilizarlas en el batch
-        PreparedStatement insertStatement = connection.prepareStatement(insertSql);
-        PreparedStatement updateStatement = connection.prepareStatement(updateSql);
-        PreparedStatement insertStatementDeptEmp = connection.prepareStatement(insertSqlDeptEmp);
-        PreparedStatement updateStatementDeptEmp = connection.prepareStatement(updateSqlDeptEmp);
-
-        // Desactivamos el autocommit para poder ejecutar el batch y hacer commit al final
-        connection.setAutoCommit(false);
-
-        for (MySqlFuel employee : employees) {
-
-            // Comprobamos si el empleado existe
-            PreparedStatement selectStatement = connection.prepareStatement(selectSql);
-            selectStatement.setInt(1, employee.getEmployeeId()); // Código del empleado
-            ResultSet resultSet = selectStatement.executeQuery();
-            resultSet.next(); // Nos movemos a la primera fila
-            int rowCount = resultSet.getInt(1);
-
-            // Si existe, actualizamos. Si no, insertamos
-            if(rowCount > 0) {
-                fillUpdateStatement(updateStatement, employee);
-                updateStatement.addBatch();
-            } else {
-                fillInsertStatement(insertStatement, employee);
-                insertStatement.addBatch();
-            }
-
-            // Comprobamos si la relación entre departamento y empleado existe
-            PreparedStatement selectStatementDeptEmp = connection.prepareStatement(selectSqlDeptEmp);
-            selectStatementDeptEmp.setInt(1, employee.getEmployeeId()); // ID del empleado
-            selectStatementDeptEmp.setString(2, employee.getDept_no()); // ID del departamento
-
-            ResultSet resultSetDeptEmp = selectStatementDeptEmp.executeQuery();
-            resultSetDeptEmp.next(); // Nos movemos a la primera fila
-            int rowCountDeptEmp = resultSetDeptEmp.getInt(1);
-
-            // Si existe, actualizamos. Si no, insertamos
-            if(rowCountDeptEmp > 0) {
-                fillUpdateStatementDeptEmp(updateStatementDeptEmp, employee);
-                updateStatementDeptEmp.addBatch();
-            } else {
-                fillInsertStatementDeptEmp(insertStatementDeptEmp, employee);
-                insertStatementDeptEmp.addBatch();
-            }
-
-            // Ejecutamos el batch cada lote de registros
-            if (++contador % lote == 0) {
-                updateStatement.executeBatch();
-                insertStatement.executeBatch();
-                updateStatementDeptEmp.executeBatch();
-                insertStatementDeptEmp.executeBatch();
-            }
-        }
-
-        // Ejecutamos el batch final
-        insertStatement.executeBatch();
-        updateStatement.executeBatch();
-        updateStatementDeptEmp.executeBatch();
-        insertStatementDeptEmp.executeBatch();
-
-        // Hacemos commit y volvemos a activar el autocommit
-        connection.commit();
-        connection.setAutoCommit(true);
-    }
-
-    /**
-     * Rellena los parámetros de un PreparedStatement para una consulta INSERT.
-     *
-     * @param statement - PreparedStatement
-     * @param employee - Empleado
-     * @throws SQLException - Error al rellenar los parámetros
-     */
-/*    private static void fillInsertStatement(PreparedStatement statement, MySqlFuel employee) throws SQLException {
-        statement.setInt(1, employee.getEmployeeId());
-        statement.setString(2, employee.getFirstName());
-        statement.setString(3, employee.getLastName());
-        statement.setString(4, employee.getGender());
-        statement.setDate(5, employee.getHireDate());
-        statement.setDate(6, employee.getBirthDate());
-    }
-
-    /**
-     * Rellena los parámetros de un PreparedStatement para una consulta UPDATE.
-     *
-     * @param statement - PreparedStatement
-     * @param employee - Empleado
-     * @throws SQLException - Error al rellenar los parámetros
-     */
-/*    private static void fillUpdateStatement(PreparedStatement statement, MySqlFuel employee) throws SQLException {
-        statement.setString(1, employee.getFirstName());
-        statement.setString(2, employee.getLastName());
-        statement.setString(3, employee.getGender());
-        statement.setDate(4, employee.getHireDate());
-        statement.setDate(5, employee.getBirthDate());
-        statement.setInt(6, employee.getEmployeeId());
-    }
-
-    /**
-     * Tabla
-     * Rellena los parámetros de un PreparedStatement para una consulta INSERT.
-     *
-     * @param statement - PreparedStatement
-     * @param employee - Empleado
-     * @throws SQLException - Error al rellenar los parámetros
-     */
-/*    private static void fillInsertStatementDeptEmp(PreparedStatement statement, MySqlFuel employee) throws SQLException {
-        statement.setInt(1, employee.getEmployeeId());
-        statement.setString(2, employee.getDept_no());
-        statement.setDate(3, employee.getFromDate());
-        statement.setDate(4, employee.getToDate());
-    }
-
-    /**
-     * Rellena los parámetros de un PreparedStatement para una consulta UPDATE.
-     *
-     * @param statement - PreparedStatement
-     * @param employee - Empleado
-     * @throws SQLException - Error al rellenar los parámetros
-     */
- /*   private static void fillUpdateStatementDeptEmp(PreparedStatement statement, MySqlFuel employee) throws SQLException {
-        statement.setDate(1, employee.getFromDate());
-        statement.setDate(2, employee.getToDate());
-        statement.setInt(3, employee.getEmployeeId());
-        statement.setString(4, employee.getDept_no());
+        statement.setName(2, fillSelectStatementIdProvToIdMun);
+        //statement.setName(2, municipalities.getPro_id());
+        statement.setString(3, municipalities.getName());
     }
 
     /**
